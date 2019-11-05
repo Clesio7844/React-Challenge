c9e5af2c2f8a32b810d1391c27ae931319b873e4
tomsenior20 <codenation@Codes-MacBook-Pro.local>
2019-11-05 11:45:01 +0000

    first

---
+++ first/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='header'>
+      <div className='section-1'>
+        <p id='sky-writing'>Sky</p>
+      </div>
+      <div className='section-2'>
+        <p id='news-writing'>Sports</p>
+      <div id='nav-icon'></div>
+      <div id='nav-icon'></div>
+      <div id='nav-icon'></div>
+      </div>
+    </div>
+
+  );
+}
+
+
+
+
+
+export default App;
+
